2e541fc87c8430b071691aa35a8ac868f6af8551
Hoang Tran <hoang.tran@vnoss.org>
2012-03-30 18:14:48 +0700

    upgrade mdadm to 3.2.3

--- package/mdadm/mdadm.mk
+++ package/mdadm/mdadm.mk
@@ -3,7 +3,7 @@
 # mdadm
 #
 #############################################################
-MDADM_VERSION:=2.6.9
+MDADM_VERSION:=3.2.3
 MDADM_SOURCE:=mdadm-$(MDADM_VERSION).tar.bz2
 MDADM_SITE:=http://www.kernel.org/pub/linux/utils/raid/mdadm
 
@@ -11,10 +11,10 @@ MDADM_MAKE_OPT = \
 	CFLAGS="$(TARGET_CFLAGS)" CC="$(TARGET_CC)" -C $(MDADM_DIR) mdadm
 
 MDADM_INSTALL_TARGET_OPT = \
-	DESTDIR=$(TARGET_DIR)/usr -C $(MDADM_DIR) install-mdadm
+	DESTDIR=$(TARGET_DIR)/usr CC="$(TARGET_CC)" -C $(MDADM_DIR) install
 
 MDADM_UNINSTALL_TARGET_OPT = \
-	DESTDIR=$(TARGET_DIR)/usr -C $(MDADM_DIR) uninstall
+	DESTDIR=$(TARGET_DIR)/usr CC="$(TARGET_CC)" -C $(MDADM_DIR) uninstall
 
 define MDADM_CONFIGURE_CMDS
 	# Do nothing
